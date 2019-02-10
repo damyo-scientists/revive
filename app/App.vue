af8211fba54977b149330e70a6d83c5fa8fe7413
Horace Velmont <dragond7@naver.com>
2019-02-10 22:00:58 +0900

    vue 내에 pixi 적용

--- app/App.vue
+++ app/App.vue
@@ -1,10 +1,22 @@
 <template>
-    <div>App test</div>
+    <div class="stage">
+    </div>
 </template>
 
 <script>
+    import SceneManager from './scenes/SceneManager';
+
     export default {
-        name: "App.vue"
+        name: "App.vue",
+        mounted() {
+            this.app = new PIXI.Application({
+                antialias: true,
+            });
+            let sceneManager = new SceneManager();
+
+            this.app.stage.addChild(sceneManager);
+            this.$el.appendChild(this.app.view);
+        }
     }
 </script>
 
