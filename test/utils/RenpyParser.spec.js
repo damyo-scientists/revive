aff0b3005e3a92cf60a3a6742be6f0ec0c8c6b7a
Horace Velmont <dragond7@naver.com>
2019-03-23 07:53:28 +0900

    테스트 케이스 생성

---
+++ test/utils/RenpyParser.spec.js
@@ -0,0 +1,10 @@
+import RenpyParser from "../../app/utils/RenpyParser";
+import assert from "assert";
+
+const parser = new RenpyParser();
+
+describe('Parse Test', function () {
+    it('should be character', function () {
+        assert.equal(true, false);
+    });
+});
